c04dd0504ff3c9be571f455407c3dfd97241028a
simon040102 <simon040102@gmail.com>
2022-11-19 00:57:21 +0800

    config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,27 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    runtime: 'experimental-edge',
-    appDir: true,
-  },
+  // experimental: {
+  //   runtime: 'experimental-edge',
+  //   appDir: true,
+  // },
   images: {
-    unoptimized: true,
+    loader: 'custom',
+    loaderFile: './my/image/loader.js',
   },
   reactStrictMode: true,
   swcMinify: true,
 };
-module.exports = nextConfig
+module.exports = nextConfig;
 
-
-/** @type {import('next').NextConfig} */
-const withTM = require("next-transpile-modules")([
-  "@fullcalendar/common",
-  "@babel/preset-react",
-  "@fullcalendar/common",
-  "@fullcalendar/daygrid",
-  "@fullcalendar/interaction",
-  "@fullcalendar/react",
-  "@fullcalendar/timegrid",
-  "@fullcalendar/list"
+const withTM = require('next-transpile-modules')([
+  '@fullcalendar/common',
+  '@babel/preset-react',
+  '@fullcalendar/common',
+  '@fullcalendar/daygrid',
+  '@fullcalendar/interaction',
+  '@fullcalendar/react',
+  '@fullcalendar/timegrid',
+  '@fullcalendar/list',
 ]);
 
 module.exports = withTM({
