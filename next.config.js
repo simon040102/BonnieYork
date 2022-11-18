8d37d9a920e2e41c504a2859b472b6dcd672ca0e
simon040102 <simon040102@gmail.com>
2022-11-18 22:35:13 +0800

    config setting

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
 const nextConfig = {
-  experimental: {
-    runtime: 'experimental-edge',
-    appDir: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  // experimental: {
+  //   runtime: 'experimental-edge',
+  //   appDir: true,
+  // },
   reactStrictMode: true,
   swcMinify: true,
 };
 module.exports = nextConfig
 
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
+};
 
 /** @type {import('next').NextConfig} */
 const withTM = require("next-transpile-modules")([
