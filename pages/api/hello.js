c8b9b34717381a8de53b5dd8ba379f81ac7061d8
simon040102 <simon040102@gmail.com>
2022-11-18 22:53:51 +0800

    config setting

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,14 +1,5 @@
-// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export const config = {
-  runtime: 'experimental-edge',
-};
-
-export default async function (req) {
-  return new Response(JSON.stringify({ name: 'John Doe' }), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
 }
