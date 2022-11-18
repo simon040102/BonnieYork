12c91b56d97ecf4ed29c446d2ab0555ce6770c17
simon040102 <simon040102@gmail.com>
2022-11-18 17:14:25 +0800

    next config setting

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-   runtime: 'experimental-edge',
- },
+  experimental: {
+    runtime: "experimental-edge",
+    appDir: true,
+  },
   reactStrictMode: true,
-}
-
+  swcMinify: true,
+};
 module.exports = nextConfig
 
 
