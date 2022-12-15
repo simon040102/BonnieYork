fc6fc44ad0e5d46915455669b33eadf6830900bd
simon040102 <simon040102@gmail.com>
2022-12-15 15:11:39 +0800

    member reserve list OK

--- components/finished.js
+++ components/finished.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 /* eslint-disable react/button-has-type */
 
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
@@ -5,45 +6,73 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
+import Link from 'next/link';
 
-const finished = () => (
-  <div className="">
-    <ul className="w-full">
-      <li className="mb-6 border-b  border-black p-5">
-        <div className="flex justify-between">
-          <h3 className="mb-2 text-2xl">xxxHairSalon</h3>
-          <button className="bg-gray-400 h-8 w-32">再次預約</button>
-        </div>
-        <div className="mb-2 flex gap-3">
-          <CalendarMonthIcon />
-          <p>2022/10/31 10:00</p>
-        </div>
-        <div className="mb-2 flex gap-3">
-          <LocationOnIcon />
-          <p>台北市大安區信義路123號</p>
-        </div>
-        <p className="mb-2">設計師：粉紅可愛豬</p>
-        <div className="flex justify-between text-xl">
-          <div className="flex gap-2">
-            <EventNoteIcon sx={{ fontSize: 30 }} />
-            <p>男士剪髮</p>
+const finished = ({ array }) => {
+  const showReserve = () =>
+    array?.map((item) => {
+      const startTime = new Date(`${item.ReserveDate} ${item.ReserveStart}`)
+        .toISOString()
+        .replace(/[\ |\.|\|\-|\\|\:|\?]/g, '');
+      const endTime = new Date(`${item.ReserveDate} ${item.ReserveEnd}`)
+        .toISOString()
+        .replace(/[\ |\.|\|\-|\\|\:|\?]/g, '');
+      const googleUrl = `https://calendar.google.com/calendar/u/0/r/eventedit?text=${item?.StoreInformation[0].StoreName} ${item.ItemName}&dates=${startTime}/${endTime}&location=%E9%AB%98%E9%9B%84%E5%B8%82%E5%89%8D%E9%87%91%E5%8D%80%E8%87%AA%E5%BC%B7%E4%BA%8C%E8%B7%AF106%E5%B7%B719%E8%99%9F`;
+      console.log(startTime, endTime);
+      return (
+        <li className="mb-6 rounded-lg bg-white p-5 shadow-lg">
+          <div className="flex justify-between">
+            <h3 className="mb-2 text-2xl font-bold">
+              {item?.StoreInformation[0].StoreName}
+            </h3>
+            {/* <Link
+              href={googleUrl}
+              className="h-8 w-32 rounded-lg bg-secondary text-white"
+            >
+              <a className="w-32 rounded-lg bg-secondary py-2 text-center text-white">
+                Google行事曆
+              </a>
+            </Link> */}
           </div>
+          <div className="mb-2 flex gap-3">
+            <CalendarMonthIcon />
+            <p>
+              {item.ReserveDate} {item.ReserveStart}
+            </p>
+          </div>
+          <div className="mb-2 flex gap-3">
+            <LocationOnIcon />
+            <p> {item?.StoreInformation[0].Address}</p>
+          </div>
+          <p className="mb-2">
+            {item?.StoreInformation[0].StaffTitle}：{item.StaffName}
+          </p>
           <div className="flex justify-between text-xl">
             <div className="flex gap-2">
-              <AccessTimeIcon sx={{ fontSize: 30 }} />
-              <p>1小時</p>
+              <EventNoteIcon sx={{ fontSize: 30, color: '#00659F' }} />
+              <p className="text-secondary">{item.ItemName}</p>
+            </div>
+            <div className="flex justify-between text-xl">
+              <div className="flex gap-2">
+                <AccessTimeIcon sx={{ fontSize: 30, color: '#00659F' }} />
+                <p className="text-secondary">{item.SpendTime}</p>
+              </div>
+            </div>
+            <div className="flex justify-between text-xl">
+              <div className="flex gap-2">
+                <PaidOutlinedIcon sx={{ fontSize: 30, color: '#00659F' }} />
+                <p className="text-secondary">{item.Price}</p>
+              </div>
             </div>
           </div>
-          <div className="flex justify-between text-xl">
-            <div className="flex gap-2">
-              <PaidOutlinedIcon sx={{ fontSize: 30 }} />
-              <p>1200</p>
-            </div>
-          </div>
-        </div>
-      </li>
-    </ul>
-  </div>
-);
+        </li>
+      );
+    });
+  return (
+    <div className="">
+      <ul className="w-full">{showReserve()}</ul>
+    </div>
+  );
+};
 
 export default finished;
--- pages/reserve.js
+++ pages/reserve.js
@@ -1,13 +1,41 @@
 /* eslint-disable react/button-has-type */
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Layout from '../modules/layout';
 
 import Finished from '../components/finished';
 import Reserved from '../components/reserved';
+import { useThem } from '../modules/context';
 
 const reserve = () => {
   const [page, setPage] = useState('reserved');
+  const [reserveArray, SetReserveArray] = useState({});
+  const { apiUrl, setLoading } = useThem();
+
+  console.log(reserveArray);
+
+  useEffect(() => {
+    const Authorization = localStorage.getItem('BonnieYork');
+    setLoading(true);
+    const config = {
+      method: 'get',
+      url: `${apiUrl}/customer/reservestate`,
+      headers: {
+        Authorization,
+      },
+    };
+    axios(config)
+      .then((res) => {
+        console.log(res);
+        setLoading(false);
+        SetReserveArray(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
   return (
     <Layout title="我的預約">
       <div className="mx-auto w-11/12 pt-10  md:w-8/12 lg:w-4/12">
@@ -16,7 +44,9 @@ const reserve = () => {
         <div className="mb-4 flex justify-center">
           <button
             className={` ${
-              page === 'reserved' ? 'text-black' : 'text-gray-500'
+              page === 'reserved'
+                ? 'border-b-2 text-secondary'
+                : ' text-unSelect'
             } mx-8`}
             onClick={() => setPage('reserved')}
           >
@@ -24,15 +54,17 @@ const reserve = () => {
           </button>
           <button
             className={` ${
-              page === 'finished' ? 'text-black' : 'text-gray-500'
+              page === 'finished'
+                ? 'border-b-2 text-secondary'
+                : ' text-unSelect'
             } mx-8`}
             onClick={() => setPage('finished')}
           >
             已完成
           </button>
         </div>
-        {page === 'reserved' && <Reserved />}
-        {page === 'finished' && <Finished />}
+        {page === 'reserved' && <Reserved array={reserveArray.UndoneReserve} />}
+        {page === 'finished' && <Finished array={reserveArray.FinishReserve} />}
       </div>
     </Layout>
   );
