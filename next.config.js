9c99b383448e925a38ed5c2a47bb1a63c30ccedd
simon040102 <simon040102@gmail.com>
2022-12-20 15:18:51 +0800

    RWD

--- next.config.js
+++ next.config.js
@@ -8,32 +8,12 @@ const withTM = require('next-transpile-modules')([
   '@fullcalendar/timegrid',
   '@fullcalendar/list',
 ]);
-
-module.exports = withTM({
-  // your custom config goes here
-  transpileModules: ['react-bulma-components'],
-  sassLoaderOptions: {
-    includePaths: ['./components'],
-  },
-  async exportPathMap(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-    return {
-      '/': { page: '/' },
-      '/login': { page: '/login' },
-      '/favorite': { page: '/favorite' },
-      '/profile': { page: '/profile' },
-      '/reserve': { page: '/reserve' },
-      '/search': { page: '/search' },
-      '/signup': { page: '/signup' },
-      '/staff': { page: '/staff' },
-      '/store': { page: '/store' },
-    };
-  },
-});
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withTM({
   reactStrictMode: true,
   images: {
     domains: ['bonnieyork.rocket-coding.com'],
   },
-};
+  withTM,
+});
 module.exports = nextConfig;
