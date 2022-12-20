fc3c53065264abd1f21c1bca2a77701d1a7864a3
simon040102 <simon040102@gmail.com>
2022-12-20 14:55:48 +0800

    RWD

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['bonnieyork.rocket-coding.com'],
-  },
-};
-module.exports = nextConfig;
-
 const withTM = require('next-transpile-modules')([
   '@fullcalendar/common',
   '@babel/preset-react',
@@ -18,26 +9,31 @@ const withTM = require('next-transpile-modules')([
   '@fullcalendar/list',
 ]);
 
-// module.exports = withTM({
-//   // your custom config goes here
-//   transpileModules: ['react-bulma-components'],
-//   sassLoaderOptions: {
-//     includePaths: ['./components'],
-//   },
-//   exportPathMap: async function (
-//     defaultPathMap,
-//     { dev, dir, outDir, distDir, buildId }
-//   ) {
-//     return {
-//       '/': { page: '/' },
-//       '/login': { page: '/login' },
-//       '/favorite': { page: '/favorite' },
-//       '/profile': { page: '/profile' },
-//       '/reserve': { page: '/reserve' },
-//       '/search': { page: '/search' },
-//       '/signup': { page: '/signup' },
-//       '/staff': { page: '/staff' },
-//       '/store': { page: '/store' },
-//     };
-//   },
-// });
+module.exports = withTM({
+  // your custom config goes here
+  transpileModules: ['react-bulma-components'],
+  sassLoaderOptions: {
+    includePaths: ['./components'],
+  },
+  async exportPathMap(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    return {
+      '/': { page: '/' },
+      '/login': { page: '/login' },
+      '/favorite': { page: '/favorite' },
+      '/profile': { page: '/profile' },
+      '/reserve': { page: '/reserve' },
+      '/search': { page: '/search' },
+      '/signup': { page: '/signup' },
+      '/staff': { page: '/staff' },
+      '/store': { page: '/store' },
+    };
+  },
+});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['bonnieyork.rocket-coding.com'],
+  },
+};
+module.exports = nextConfig;
